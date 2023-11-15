d68cc9e3dde5d9e79827e694b2a7c939f04831dd
Nafla Fadia <naflafadia0@gmail.com>
2023-11-15 15:44:51 +0700

    menghapus company

--- day7/js/testimonials.js
+++ day7/js/testimonials.js
@@ -19,11 +19,10 @@ document.addEventListener('click', function (e) {
 // Testimonials
 
 class Testimonials {
-    constructor(image, quotes, author, company, rate) {
+    constructor(image, quotes, author, rate) {
         this.image = image;
         this.quotes = quotes;
         this.author = author;
-        this.company = company;
         this.rate = rate;
     }
 
@@ -35,7 +34,6 @@ class Testimonials {
         <h3>${this.quotes}</h3>
         <div class="content-card">
             <h2>${this.author}</h2>
-            <h4>${this.company}</h4>
             <div class="card-rate">
                 <h2>${this.rate}</h2>
                 <img src="./img/star.png">
@@ -46,11 +44,11 @@ class Testimonials {
     }
 }
 
-const testi1 = new Testimonials('./img/woman5.jpg', `"Layanan pelanggan luar biasa! Respon cepat dan solusi yang memuaskan."`, 'Seraphina Blake', 'QuantumForge Solutions', '5');
-const testi2 = new Testimonials('./img/woman2.jpg', `"Produk ini memenuhi harapan saya. Kualitasnya luar biasa, sangat direkomendasikan!"`, 'Aria Steele', 'Stellar Innovations Co.', '4');
-const testi3 = new Testimonials('./img/woman3.jpg', `"Pengalaman belanja online yang menyenangkan. Mudah, cepat, dan akurat."`, 'Zara Knight', 'VelocitySynergy Enterprises', '3');
-const testi4 = new Testimonials('./img/woman4.jpg', `"Dukungan pelanggan luar biasa. Prosesnya lancar dan hasilnya memuaskan."`, 'Luna Everhart', 'Nexus Dynamics Group', '4');
-const testi5 = new Testimonials('./img/woman1.jpg', `"Produk revolusioner! Mempermudah pekerjaan dan sangat efisien."`, 'Jocelyn Phoenix', 'ApexTech Ventures', '5');
+const testi1 = new Testimonials('./img/woman5.jpg', `"Layanan pelanggan luar biasa! Respon cepat dan solusi yang memuaskan."`, 'Seraphina Blake', '5');
+const testi2 = new Testimonials('./img/woman2.jpg', `"Produk ini memenuhi harapan saya. Kualitasnya luar biasa, sangat direkomendasikan!"`, 'Aria Steele', '4');
+const testi3 = new Testimonials('./img/woman3.jpg', `"Pengalaman belanja online yang menyenangkan. Mudah, cepat, dan akurat."`, 'Zara Knight','3');
+const testi4 = new Testimonials('./img/woman4.jpg', `"Dukungan pelanggan luar biasa. Prosesnya lancar dan hasilnya memuaskan."`, 'Luna Everhart','4');
+const testi5 = new Testimonials('./img/woman1.jpg', `"Produk revolusioner! Mempermudah pekerjaan dan sangat efisien."`, 'Jocelyn Phoenix', '5');
 
 const testi = [testi1, testi2, testi3, testi4, testi5];
 
