17afb480ee2ce575f80268877a0fc10984d76207
Nafla Fadia <naflafadia0@gmail.com>
2023-11-15 07:42:17 +0700

    memperbaiki project

--- day7/js/testimonials.js
+++ day7/js/testimonials.js
@@ -38,7 +38,7 @@ class Testimonials {
             <h4>${this.company}</h4>
             <div class="card-rate">
                 <h2>${this.rate}</h2>
-                <img src="./day7/img/star.png">
+                <img src="./img/star.png">
             </div>
         </div>
         </div>
@@ -46,11 +46,11 @@ class Testimonials {
     }
 }
 
-const testi1 = new Testimonials('./day7/img/woman5.jpg', `"Layanan pelanggan luar biasa! Respon cepat dan solusi yang memuaskan."`, 'Seraphina Blake', 'QuantumForge Solutions', '5');
-const testi2 = new Testimonials('./day7/img/woman2.jpg', `"Produk ini memenuhi harapan saya. Kualitasnya luar biasa, sangat direkomendasikan!"`, 'Aria Steele', 'Stellar Innovations Co.', '4');
-const testi3 = new Testimonials('./day7/img/woman3.jpg', `"Pengalaman belanja online yang menyenangkan. Mudah, cepat, dan akurat."`, 'Zara Knight', 'VelocitySynergy Enterprises', '3');
-const testi4 = new Testimonials('./day7/img/woman4.jpg', `"Dukungan pelanggan luar biasa. Prosesnya lancar dan hasilnya memuaskan."`, 'Luna Everhart', 'Nexus Dynamics Group', '4');
-const testi5 = new Testimonials('./day7/img/woman1.jpg', `"Produk revolusioner! Mempermudah pekerjaan dan sangat efisien."`, 'Jocelyn Phoenix', 'ApexTech Ventures', '5');
+const testi1 = new Testimonials('./img/woman5.jpg', `"Layanan pelanggan luar biasa! Respon cepat dan solusi yang memuaskan."`, 'Seraphina Blake', 'QuantumForge Solutions', '5');
+const testi2 = new Testimonials('./img/woman2.jpg', `"Produk ini memenuhi harapan saya. Kualitasnya luar biasa, sangat direkomendasikan!"`, 'Aria Steele', 'Stellar Innovations Co.', '4');
+const testi3 = new Testimonials('./img/woman3.jpg', `"Pengalaman belanja online yang menyenangkan. Mudah, cepat, dan akurat."`, 'Zara Knight', 'VelocitySynergy Enterprises', '3');
+const testi4 = new Testimonials('./img/woman4.jpg', `"Dukungan pelanggan luar biasa. Prosesnya lancar dan hasilnya memuaskan."`, 'Luna Everhart', 'Nexus Dynamics Group', '4');
+const testi5 = new Testimonials('./img/woman1.jpg', `"Produk revolusioner! Mempermudah pekerjaan dan sangat efisien."`, 'Jocelyn Phoenix', 'ApexTech Ventures', '5');
 
 const testi = [testi1, testi2, testi3, testi4, testi5];
 
